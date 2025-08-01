94a6d33732d78d24e733549965f75ee7bc847ed5
mithun0619 <prajapatimithun2151@gmail.com>
2025-08-01 08:09:28 +0530

    Time: 3 ms (48.64%) | Memory: 9.2 MB (21.54%) - LeetSync

---
+++ 52-n-queens-ii/n-queens-ii.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+
+       bool isSafe(vector<string> &board,int row,int col,int n){
+        //horizontal checking
+        for(int j=0;j<n;j++){
+            if(board[row][j]=='Q'){
+                return false;
+            }
+        }
+
+        //vertical check
+        for(int i=0;i<n;i++){
+            if(board[i][col]=='Q'){
+                return false;
+            }
+        }
+
+        //Diagonally Check
+        //left upper diagonal
+        for(int i=row,j=col;i>=0 && j>=0;i--,j--){
+            if(board[i][j]=='Q'){
+                return false;
+            }
+        }
+
+        //Right upper Diagonal
+        for(int i=row,j=col;i>=0 && j<n; i--,j++){
+            if(board[i][j]=='Q'){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    void nQueens(vector<string> &board, int row, int n, vector<vector<string>> &ans)    {
+       
+    if (row == n) {
+        ans.push_back(board);
+        return; // Don't forget to return here!
+    }
+
+    for (int j = 0; j < n; j++) {
+        if (isSafe(board, row, j, n)) {
+            board[row][j] = 'Q';
+            nQueens(board, row + 1, n, ans);
+            board[row][j] = '.';
+        }
+    }
+}
+
+    int totalNQueens(int n) {
+        vector<string>board(n,string(n,'.'));
+        vector<vector<string>> ans;
+        nQueens(board,0,n,ans);
+        int count=ans.size();
+        return count;
+    }
+
+    
+};
