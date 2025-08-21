4d898307fb7d8b887b025650136daccfe38d253b
mithun0619 <prajapatimithun2151@gmail.com>
2025-08-21 21:35:43 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (80.48%) - LeetSync

--- 338-counting-bits/counting-bits.cpp
+++ 338-counting-bits/counting-bits.cpp
@@ -3,7 +3,7 @@ using namespace std;
 class Solution {
 public:
     vector<int> countBits(int n) {
-        vector<int> ans(n + 1);
+        vector<int> ans(n + 1,0);
         for (int i = 1; i <= n; i++) {
             ans[i] = ans[i >> 1] + (i & 1);
         }
