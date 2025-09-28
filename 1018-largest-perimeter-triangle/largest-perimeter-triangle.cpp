7199f5d724a329cda19e0704e8363cbacc678b91
mithun0619 <prajapatimithun2151@gmail.com>
2025-09-28 07:53:59 +0530

    Time: 0 ms (100.00%) | Memory: 25.6 MB (76.13%) - LeetSync

--- 1018-largest-perimeter-triangle/largest-perimeter-triangle.cpp
+++ 1018-largest-perimeter-triangle/largest-perimeter-triangle.cpp
@@ -4,7 +4,7 @@ public:
         sort(nums.begin(), nums.end(), greater<int>());
         for (int i = 0; i < nums.size() - 2; i++)
             if (nums[i + 1] + nums[i + 2] > nums[i])
-                return nums[i+2] + nums[i + 1] + nums[i];
+                return nums[i+2] + nums[i ] + nums[i + 1];
         return 0;
     }
 };
