eabcb0384d85cf90d6d5934b0a08ff2ab0055aee
mithun0619 <prajapatimithun2151@gmail.com>
2026-04-11 08:15:06 +0530

    Time: 3 ms (94.99%) | Memory: 22.9 MB (77.03%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -13,19 +13,19 @@ public:
         int low=1;
         int maxpile=*max_element(piles.begin(),piles.end());
         int high=maxpile;
-        int ans=maxpile;
+        
         while(low<=high){
             int mid=low+(high-low)/2;
 
             long long th=totalhr(piles,mid);
 
             if(th<=h){
-                ans=mid;
+            
                 high=mid-1;
             }else{
                 low=mid+1;
             }
         }
-        return ans;
+        return low;
     }
 };
