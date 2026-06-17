adcdefca9db44dda05b766e884733069b01904bb
mithun0619 <prajapatimithun2151@gmail.com>
2026-06-17 07:31:31 +0530

    Time: 2 ms (71.04%) | Memory: 9.3 MB (74.99%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.cpp
+++ 12-integer-to-roman/integer-to-roman.cpp
@@ -1,14 +1,20 @@
 class Solution {
 public:
     string intToRoman(int num) {
-        vector<string> thousand={"","M","MM","MMM"};
-        vector<string> hundred={"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
-        vector<string> tens={"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};
-        vector<string> ones={"","I","II","III","IV","V","VI","VII","VIII","IX"};
+        if(num >= 1000) return "M" + intToRoman(num - 1000);
+        if(num >= 900)  return "CM" + intToRoman(num - 900);
+        if(num >= 500)  return "D" + intToRoman(num - 500);
+        if(num >= 400)  return "CD" + intToRoman(num - 400);
+        if(num >= 100)  return "C" + intToRoman(num - 100);
+        if(num >= 90)   return "XC" + intToRoman(num - 90);
+        if(num >= 50)   return "L" + intToRoman(num - 50);
+        if(num >= 40)   return "XL" + intToRoman(num - 40);
+        if(num >= 10)   return "X" + intToRoman(num - 10);
+        if(num >= 9)    return "IX" + intToRoman(num - 9);
+        if(num >= 5)    return "V" + intToRoman(num - 5);
+        if(num >= 4)    return "IV" + intToRoman(num - 4);
+        if(num >= 1)    return "I" + intToRoman(num - 1);
 
-        return thousand[num/1000]+
-                hundred[(num%1000)/100]+
-                tens[(num%100)/10]+
-                ones[(num%10)];
+        return "";
     }
 };
